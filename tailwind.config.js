2a2c48c693578b345d5df80d41cf79e85d65464d
Ki2lian <64066124+Ki2lian@users.noreply.github.com>
2024-02-23 13:58:24 +0100

    style: Update styles and tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,23 @@ module.exports = {
     content: ['./pages/**/*.{ts,tsx}', './components/**/*.{ts,tsx}', './app/**/*.{ts,tsx}', './src/**/*.{ts,tsx}'],
     prefix: '',
     theme: {
+        fontSize: {
+            sm: '0.750rem',
+            base: '1rem',
+            xl: '1.333rem',
+            '2xl': '1.777rem',
+            '3xl': '2.369rem',
+            '4xl': '3.158rem',
+            '5xl': '4.210rem',
+        },
+        fontFamily: {
+            heading: 'Lato',
+            body: 'Lato',
+        },
+        fontWeight: {
+            normal: '400',
+            bold: '700',
+        },
         container: {
             center: true,
             padding: '2rem',
