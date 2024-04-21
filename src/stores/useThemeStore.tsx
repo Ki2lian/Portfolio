c69f053d12481a650ac14c175fa49814e8a4c8c1
Ki2lian <64066124+Ki2lian@users.noreply.github.com>
2024-04-21 11:01:50 +0200

    refactor: Rename ThemeState interface to IThemeState

--- src/stores/useThemeStore.tsx
+++ src/stores/useThemeStore.tsx
@@ -1,12 +1,12 @@
 import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
-interface ThemeState {
+interface IThemeState {
     theme: 'light' | 'dark';
     toggleTheme: ()=> void;
   }
 
-export const useThemeStore = create<ThemeState>()(
+export const useThemeStore = create<IThemeState>()(
     persist(
         (set) => ({
             theme: 'light',
