05839ee226bd7d4624c906e3e54e73e98877ae55
Ki2lian <64066124+Ki2lian@users.noreply.github.com>
2024-04-29 15:11:41 +0200

    feat: Add contact section

---
+++ src/components/ContactSection.tsx
@@ -0,0 +1,29 @@
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import { GithubIcon } from '@/components/icons/GithubIcon';
+import { TwitterIcon } from '@/components/icons/TwitterIcon';
+import { DiscordIcon } from '@/components/icons/DiscordIcon';
+
+export const ContactSection: React.FC<React.HTMLProps<HTMLDivElement>> = ({ className, ...props }) => {
+    const { t } = useTranslation();
+
+    return (
+        <section className={ className } { ...props }>
+            <h2 className="text-2xl">{ t('contact.title') }</h2>
+            <div className="flex items-center gap-6 pt-8">
+                <Link to="https://github.com/ki2lian" target="_blank" rel="noopener noreferrer">
+                    <GithubIcon className="transition-transform hover:scale-110" size={ 36 } />
+                </Link>
+                <Link to="https://twitter.com/ki2lianm" target="_blank" rel="noopener noreferrer">
+                    <TwitterIcon className="transition-transform hover:scale-110" size={ 36 } />
+                </Link>
+                <Link to="https://discord.gg/FMb2x7v7" target="_blank" rel="noopener noreferrer">
+                    <DiscordIcon className="transition-transform hover:scale-110" size={ 36 } />
+                </Link>
+                <Link to="mailto:killian@ki2lian.fr" rel="noopener noreferrer" className="underline">
+                    killian@ki2lian.fr
+                </Link>
+            </div>
+        </section>
+    );
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,4 @@
+import { ContactSection } from '@/components/ContactSection';
 import { HeroSection } from '@/components/HeroSection';
 import { ProjectsSection } from '@/components/projects/ProjectsSection';
 import { SkillsSection } from '@/components/skills/SkillsSection';
@@ -5,9 +6,10 @@ import { SkillsSection } from '@/components/skills/SkillsSection';
 export const HomePage = () => {
     return (
         <>
-            <HeroSection className="relative m-auto max-w-3xl py-5 lg:py-7" />
-            <ProjectsSection className="relative m-auto max-w-3xl py-5 lg:py-7" id="projects" />
-            <SkillsSection className="py-5 lg:py-7" id="skills" />
+            <HeroSection className="relative m-auto max-w-3xl py-7 lg:py-10" />
+            <ProjectsSection className="relative m-auto max-w-3xl py-7 lg:py-10" id="projects" />
+            <SkillsSection className="py-7 lg:py-10" id="skills" />
+            <ContactSection className="relative m-auto mb-7 max-w-3xl py-7 lg:mb-10 lg:py-10" id="contact" />
         </>
     );
 };
